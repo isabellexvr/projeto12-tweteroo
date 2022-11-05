41650a2a391ae474197959300b67a3a8e9e59f20
isabellexvr <isabellexavier59@gmail.com>
2022-11-05 00:48:26 -0300

    init

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.listen(5000, () => {
+    console.log("App running in port: 5000");
+})
+
